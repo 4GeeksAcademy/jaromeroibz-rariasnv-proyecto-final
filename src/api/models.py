17baa7d992527e1cbbbc5943d1c0e365ea7769c1
rariasnav <137901995+rariasnav@users.noreply.github.com>
2023-11-02 01:40:32 +0000

    commit

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,26 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Petitioner (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), unique=False, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    email_address = db.Column(db.String(120), unique=True, nullable=False)
+    offer_services = db.Column(db.String(120), unique=False, nullable=False)
+    rating = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Petitioner {self.email_address}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "phone_number": self.phone_number,
+            "address": self.address,
+            "email_address": self.email_address,
+            "offer_services": self.offer_services,
+            "rating": self.rating
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Petitioner
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,71 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/petitioner', methods=['GET'])
+def get_all_petitioner():
+    all_petitioner = Petitioner.query.all()
+    result = list(map(lambda petitioner: petitioner.serialize() ,all_petitioner))
+
+    return jsonify(result), 200
+
+@api.route('/petitioner/<int:petitioner_id>', methods=['GET'])
+def get_one_particular_petitioner(petitioner_id):
+    particular_petitioner = Petitioner.query.filter_by(id=petitioner_id).first()
+
+    return jsonify(particular_petitioner.serialize()), 200
+
+@api.route('/petitioner', methods=['POST'])
+def create_petitioner():
+    body = request.get_json()
+    petitioner = Petitioner(
+        full_name = body['full_name'],
+        phone_number = body['phone_number'],
+        address = body['address'],
+        email_address = body['email_address'],
+        offer_services = body['offer_services'],
+        rating = body['rating']
+    )    
+
+    db.session.add(petitioner)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Petitioner created"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/petitioner/<int:petitioner_id>', methods=['DELETE'])
+def delete_one_particular_petitioner(petitioner_id):
+    petitioner_to_delete = Petitioner.query.filter_by(id=petitioner_id).first()
+
+    db.session.delete(petitioner_to_delete)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Petitioner deleted"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/petitioner/<int:petitioner_id>', methods=['PUT'])
+def update_one_particular_petitioner(petitioner_id):
+    petitioner_to_update = Petitioner.query.filter_by(id=petitioner_id).first()
+    body = request.get_json()
+
+    petitioner_to_update.full_name = body['full_name']
+    petitioner_to_update.phone_number = body['phone_number']
+    petitioner_to_update.address = body['address']
+    petitioner_to_update.email_address = body['email_address']
+    petitioner_to_update.offer_services = body['offer_services']
+    petitioner_to_update.rating = body['rating']
+
+    db.session.commit()
+
+    response_body = {
+        "msg": "Petitioner updated"
+    }
+
+    return jsonify(response_body), 200
+
